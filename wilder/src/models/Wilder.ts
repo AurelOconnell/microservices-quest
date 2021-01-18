edbdebc5a57ccdb04b4119657a0c8368e48cfd1c
Thomas Culdaut <thomas.culdaut@gmail.com>
2021-01-18 15:22:50 +0100

    transform to JSON

--- wilder/src/models/Wilder.ts
+++ wilder/src/models/Wilder.ts
@@ -7,10 +7,28 @@ export interface IWilder {
 
 interface WilderDoc extends IWilder, Document {}
 
-const WilderSchema = new Schema<WilderDoc>({
-  name: { type: String, unique: true },
-  city: String,
-});
+const WilderSchema = new Schema<WilderDoc>(
+  {
+    name: { type: String, unique: true },
+    city: String,
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        const jsonRet = {
+          ...ret,
+          // eslint-disable-next-line no-underscore-dangle
+          id: ret._id,
+        };
+        // eslint-disable-next-line no-underscore-dangle
+        delete jsonRet._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete jsonRet.__v;
+        return jsonRet;
+      },
+    },
+  }
+);
 
 const Wilder = mongoose.model<WilderDoc>('wilder', WilderSchema);
 
